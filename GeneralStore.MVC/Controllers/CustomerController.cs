8b8accd8335d7e5d4f996702688f73a5aa624fcf
Julian <arboledajulian@outlook.com>
2021-03-23 17:30:14 -0400

    added CRUD functionalities for Customers.cs

--- GeneralStore.MVC/Controllers/CustomerController.cs
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -1,127 +1,120 @@
 ﻿using GeneralStore.MVC.Models;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
 namespace GeneralStore.MVC.Controllers
 {
-    public class ProductController : Controller
+    public class CustomerController : Controller
     {
-        // Add the application DB Context (link to the database)
         private ApplicationDbContext _db = new ApplicationDbContext();
-
-
-        // GET: Product
+        // GET: Customer
         public ActionResult Index()
         {
-            // See below (modifying ApplicatoinDbContext class)
-            List<Product> productList = _db.Products.ToList();
-            List<Product> orderedList = productList.OrderBy(prod => prod.Name).ToList();
+            List<Customer> customerList = _db.Customers.ToList();
+            List<Customer> orderedList = customerList.OrderBy(cust => cust.LastName).ToList();
             return View(orderedList);
         }
 
-        //GET: Product
+        // GET: Customer
+        [HttpGet]
         public ActionResult Create()
         {
             return View();
         }
 
-
-        //POST: Product
+        // POST: Customer
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Customer customer)
         {
             if (ModelState.IsValid)
             {
-                _db.Products.Add(product);
+                _db.Customers.Add(customer);
                 _db.SaveChanges();
-                return RedirectToAction("Index");
+                RedirectToAction("Index");
             }
-
-            return View(product);
+            return View(customer);
         }
 
         // GET: Delete
-        // Product/Delete/{id}
-        [HttpGet]
+        // Customer/Delete/{id}
+        [HttpDelete]
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
             }
-
-            Product product = _db.Products.Find(id);
-            if (product == null)
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
             {
                 return HttpNotFound();
             }
-
-            return View(product);
+            return View(customer);
         }
 
         // POST: Delete
-        // Product/Delete/{id}
+        // Customer/Delete/{id}
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
         {
-            Product product = _db.Products.Find(id);
-            _db.Products.Remove(product);
+            Customer customer = _db.Customers.Find(id);
+            _db.Customers.Remove(customer);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        // GET : Edit
-        // Product/Edit/{id}
+        // GET: Edit
+        // Customer/Edit/{id}
+        [HttpGet]
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
             }
-            Product product = _db.Products.Find(id);
-            if (product == null)
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
             {
                 return HttpNotFound();
             }
-            return View(product);
+            return View(customer);
         }
 
-        // POST : Edit// Product/Edit/{id}
+        // POST: Edit
+        // Customer/Edit/{id}
         [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(Product product)
+        public ActionResult Edit(Customer customer)
         {
             if (ModelState.IsValid)
             {
-                _db.Entry(product).State = EntityState.Modified;
+                _db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(product);
+            return View(customer);
         }
 
-        // GET : Details
-        // Product/Details/{id}
+        // GET: Details
+        // Customer/Edit/{id}
+        [HttpGet]
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
             }
-            Product product = _db.Products.Find(id);
-
-            if (product == null)
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
             {
                 return HttpNotFound();
             }
 
-            return View(product);
+            return View(customer);
         }
     }
 }
