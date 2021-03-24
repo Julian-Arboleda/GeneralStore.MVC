ad6ef6b21d7bfa034c96ec73b05e45ee27201c4d
Julian <arboledajulian@outlook.com>
2021-03-23 22:06:49 -0400

    added Transaction functionality

---
+++ GeneralStore.MVC/Controllers/TransactionController.cs
@@ -0,0 +1,42 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Transaction
+        public ActionResult Index(int id)
+        {
+            List<Transaction> transactionList = _db.Transactions.ToList();
+            List<Transaction> orderedList = transactionList.OrderBy(trans => trans.DateOfTransaction).ToList();
+            return View(orderedList);
+        }
+
+        // GET: Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        public ActionResult Create(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Transactions.Add(transaction);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+                return View(transaction);
+        }
+    }
+}
---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        public int Quantity { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateOfTransaction { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual Product Product{ get; set; }
+    }
+}
+
+
+
+/* [Required]
+[ForeignKey(nameof(Product))]
+public int ProductId { get; set; }
+public virtual Product Product { get; set; }
+
+[Required]
+public int CustomerId { get; set; }
+[ForeignKey(nameof(CustomerId))]
+public virtual Customer Customer { get; set; }*/
